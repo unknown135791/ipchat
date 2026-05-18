a8b3456bc9205c8a20f83a04c3dd024ffc522122
unknown135791 <mohit1357913579@gmail.com>
2026-05-18 21:04:23 +0530

    Add files via upload

--- server.js
+++ server.js
@@ -30,13 +30,7 @@ const roomRegex = /^[a-zA-Z0-9_-]{1,20}$/;
 
 function sanitizeInput(str) {
     if (typeof str !== "string") return "";
-    return str
-        .replace(/&/g, "&amp;")
-        .replace(/</g, "&lt;")
-        .replace(/>/g, "&gt;")
-        .replace(/"/g, "&quot;")
-        .replace(/'/g, "&#x27;")
-        .replace(/\//g, "&#x2F;");
+    return str;
 }
 
 // In-memory rate limiting tracking (socket.id -> timestamps)
